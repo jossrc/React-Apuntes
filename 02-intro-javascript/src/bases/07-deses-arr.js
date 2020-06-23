7462a5f56b46b5c3e63cefed0080bbba56020a7a
Jhoss <kuro.rc10@gmail.com>
2020-06-23 13:39:53 -0500

    Desestructuración de arreglos

---
+++ 02-intro-javascript/src/bases/07-deses-arr.js
@@ -0,0 +1,43 @@
+const characters = ['Goku', 'Vegeta', 'Trunks'];
+
+const [p1, , trunks] = characters;
+console.log(p1, trunks);
+
+const returnArray = () => {
+  return ['ABC', 123];
+};
+
+const [letters] = returnArray();
+
+console.log(letters);
+
+// Tarea
+// 1. El primer valor del arr se llamará name
+// 2. El segundo valor del arr se llamará setName
+
+/* 
+const useState = (value) => {
+  return [
+    value,
+    () => {
+      console.log('Hello World');
+    },
+  ];
+};
+
+const arr = useState('Goku');
+console.log(arr);
+ */
+
+const useState = (value) => {
+  return [
+    value,
+    () => {
+      console.log('Hello World');
+    },
+  ];
+};
+
+const [name, setName] = useState('Goku');
+console.log(name);
+setName();
