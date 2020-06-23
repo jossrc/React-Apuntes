5bdf6355ac522cc853260e979c96e96c44db9ab9
Jhoss <kuro.rc10@gmail.com>
2020-06-23 10:24:57 -0500

    Variables y constantes

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,18 @@
+console.log('Hello World with React');
+
+const name = 'Jose';
+const lastName = 'Robles';
+
+let diceValue = 5;
+diceValue = 4;
+
+console.log({ name, lastName, diceValue });
+
+// var NO se debe usar
+
+if (true) {
+  const name = 'Fernando';
+  console.log({ name });
+}
+
+console.log({ diceValue });
