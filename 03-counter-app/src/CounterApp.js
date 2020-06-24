c8f0c89a6abd494b283dbeb551bc9beb32aacf0b
Jhoss <kuro.rc10@gmail.com>
2020-06-24 13:26:15 -0500

    Eventos en React - Evento Click

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -2,21 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const CounterApp = ({ value }) => {
+  const handleAdd = (e) => {
+    console.log(e);
+  };
+
   return (
     <>
       <h1>CounterApp</h1>
-      <hr/>
-      <h2> <i>{value}</i> </h2>
+      <hr />
+      <h2>
+        <i>{value}</i>
+      </h2>
+
+      <button onClick={ handleAdd }>
+        Click <b>+1</b>
+      </button>
     </>
   );
 };
 
 CounterApp.propTypes = {
-    value: PropTypes.number
-}
+  value: PropTypes.number,
+};
 
 CounterApp.defaultProps = {
-    value: 0
-}
+  value: 0,
+};
 
 export default CounterApp;
