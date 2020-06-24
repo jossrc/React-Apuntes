5901afde8e2158e29aedefd661a8f1b71efcee32
Jhoss <kuro.rc10@gmail.com>
2020-06-24 12:54:14 -0500

    PropTypes

--- 03-counter-app/src/FirstApp.js
+++ 03-counter-app/src/FirstApp.js
@@ -1,13 +1,17 @@
 import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
 
-const FirstApp = ({ greeting = 'Bienvenido' }) => {
+const FirstApp = ({ greeting }) => {
   return (
     <Fragment>
       <h1>CounterApp</h1>
-      {/* <p>{props.greeting}</p> */}
       <p>{greeting}</p>
     </Fragment>
   );
 };
+
+FirstApp.propTypes = {
+    greeting: PropTypes.string.isRequired
+}
 
 export default FirstApp;
