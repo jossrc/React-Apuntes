52c30e578c7446bdc6aabd85f113c1921f026dae
Jhoss <kuro.rc10@gmail.com>
2020-06-24 13:03:05 -0500

    DefaultProps

--- 03-counter-app/src/FirstApp.js
+++ 03-counter-app/src/FirstApp.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
-const FirstApp = ({ greeting }) => {
+const FirstApp = ({ greeting, subTitle }) => {
   return (
     <Fragment>
       <h1>CounterApp</h1>
+      <hr/>
+      <h4> <i>{subTitle}</i> </h4>
       <p>{greeting}</p>
     </Fragment>
   );
@@ -12,6 +14,10 @@ const FirstApp = ({ greeting }) => {
 
 FirstApp.propTypes = {
     greeting: PropTypes.string.isRequired
+}
+
+FirstApp.defaultProps = {
+    subTitle : 'Cuenta con nosotros'
 }
 
 export default FirstApp;
