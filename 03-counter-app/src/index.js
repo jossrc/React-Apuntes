0b893080d62b21c4d14bda8038a287abcd6228f6
Jhoss <kuro.rc10@gmail.com>
2020-06-24 11:25:20 -0500

    Creando el primer componente

--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const greeting = <h1>Hello World</h1>
+import FirstApp from './FirstApp';
+
+import './index.css';
+
 const divRoot = document.getElementById('root');
 
-ReactDOM.render(greeting, divRoot)
+ReactDOM.render(<FirstApp/>, divRoot)
