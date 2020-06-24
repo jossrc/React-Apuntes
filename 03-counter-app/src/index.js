c54e89ed5a1ce240c5c561a9edffb5b60729785d
Jhoss <kuro.rc10@gmail.com>
2020-06-24 11:13:34 -0500

    Hola Mundo en React

--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -1,1 +1,7 @@
-console.log('Hello World')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const greeting = <h1>Hello World</h1>
+const divRoot = document.getElementById('root');
+
+ReactDOM.render(greeting, divRoot)
