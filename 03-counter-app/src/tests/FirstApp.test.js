bc805bc764567f1a788d831ea7afa4a56de9cf61
Jhoss <kuro.rc10@gmail.com>
2020-06-25 15:07:04 -0500

    Instalando y usando Enzyme - enzyme-to-json. Creando la carpeta __snapshots__

--- 03-counter-app/src/tests/FirstApp.test.js
+++ 03-counter-app/src/tests/FirstApp.test.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
 
 import FirstApp from '../FirstApp';
+import { shallow } from 'enzyme';
 
 describe('Pruebas en el componente FirstApp', () => {
-  test('greeting : Debe de mostrar el saludo Bienvenido a React', () => {
+  test('FirstApp : Debe de mostar <FirstApp/> correctamente', () => {
     const greetingTest = 'Bienvenido a React';
-    // wrapper = render(<FirstApp greeting={greetingTest} />)
-    // wrapper.getByText
-    const { getByText } = render(<FirstApp greeting={greetingTest} />);
+    const wrapper = shallow(<FirstApp greeting={greetingTest} />);
 
-    expect(getByText(greetingTest)).toBeInTheDocument();
+    expect(wrapper).toMatchSnapshot();
   });
 });
